c7dc0d86a7ac54d44868bebc1a89a439912fa701
voidiz <voidqt@hotmail.com>
2018-11-24 14:27:12 +0100

    added setup.py

--- horriblesubs_batch_downloader/__main__.py
+++ horriblesubs_batch_downloader/__main__.py
@@ -3,16 +3,16 @@
 """Horriblesubs Batch Downloader
 
 Usage:
-    horriblesubs_batch_downloader <url> (-a | <episode> | (<start> <end>)) [-r <res>]
-    horriblesubs_batch_downloader -n <show_name> (-a | <episode> | (<start> <end>)) [-r <res>]
-    horriblesubs_batch_downloader -h | --help
+    hsbd <url> (-a | <episode> | (<start> <end>)) [-r <res>]
+    hsbd -n <show_name> (-a | <episode> | (<start> <end>)) [-r <res>]
+    hsbd -h | --help
 
 Examples:
     # Download all episodes in the highest quality from a url:
-    horriblesubs_batch_downloader "https://horriblesubs.info/shows/kuzu-no-honkai" -a
+    hsbd "https://horriblesubs.info/shows/kuzu-no-honkai" -a
 
     # Download episodes 1 through 5 in 720p using a show name:
-    horriblesubs_batch_downloader -n "Kuzu no Honkai" 1 5 -r 720
+    hsbd -n "Kuzu no Honkai" 1 5 -r 720
 
 Options:
     -h, --help          Show this screen.
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='horriblesubs_batch_downloader',
+    version='1.0',
+    description='A module that fetches magnet links from HorribleSubs and opens them',
+    author='voidiz',
+    packages=['horriblesubs_batch_downloader'],
+    install_requires=['docopt', 'requests', 'beautifulsoup4'],
+    entry_points={
+        'console_scripts': [
+            'hsbd=horriblesubs_batch_downloader.__main__:main'
+        ]
+    },
+)
